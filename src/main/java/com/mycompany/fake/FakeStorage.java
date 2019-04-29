52ff6231f68be481cfac22649234394fd936d9a6
Karl <Karl@DESKTOP-V5FIRD5>
2019-04-29 10:27:25 +0300

    первый пример по работе с jsp , пока без базы данных

--- src/main/java/com/mycompany/fake/FakeStorage.java
+++ src/main/java/com/mycompany/fake/FakeStorage.java
@@ -1,10 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.mycompany.fake;
 
+package com.mycompany.fake;
 
 import com.mycompany.models.User;
 
@@ -18,32 +13,28 @@ import java.util.List;
 public class FakeStorage {
     // переменная, которая хранит ссылку на единственный экземпляр объекта класса FakeStorage
     private static final FakeStorage storage;
-
     // статически инициализатор, создающий объект класса FakeStorage. Вызывается один раз при загрузке класса в JVM
     static {
         storage = new FakeStorage();
     }
-
     // поле-список, хранящее список пользователей системы
     private List<User> users;
 
     // приватный констуктор, выполняющий инициализацию списка
     private FakeStorage() {
         this.users = new ArrayList<>();
-        User user = new User("Marsel", "qwerty007", LocalDate.parse("1994-02-02"));
-        User user1 = new User("Marsel1", "qwerty007", LocalDate.parse("1994-02-02"));
-        User user2 = new User("Marsel2", "qwerty007", LocalDate.parse("1994-02-02"));
+        User user = new User("Kirill", "222", LocalDate.parse("1975-02-02"));
+        User user1 = new User("Trofim", "222", LocalDate.parse("1985-02-02"));
+        User user2 = new User("Valentina", "222", LocalDate.parse("1995-02-02"));
 
         users.add(user);
         users.add(user1);
         users.add(user2);
     }
-
     // метод, предоставляющий доступ к объекту класса
     public static FakeStorage storage() {
         return storage;
     }
-
     // метод, возвращающий список пользователей
     public List<User> users() {
         return users;
