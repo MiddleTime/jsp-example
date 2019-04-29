27dc392e86e5a8277cf605fe225dfa9691eda668
Karl <Karl@DESKTOP-V5FIRD5>
2019-04-29 10:32:47 +0300

    первый пример по работе с jsp , пока без базы данных

--- src/main/java/com/mycompany/repositories/UsersRepositoryInMemoryImpl.java
+++ src/main/java/com/mycompany/repositories/UsersRepositoryInMemoryImpl.java
@@ -1,21 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.repositories;
 
 import com.mycompany.fake.FakeStorage;
 import com.mycompany.models.User;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
-/**
- *
- * @author Karl
- */
+
 public class UsersRepositoryInMemoryImpl implements UsersRepository{
+    @Override
     public List<User> findAll() {
         return FakeStorage.storage().users();
     }
